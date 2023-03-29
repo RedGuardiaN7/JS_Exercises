7a68cce8c6e2c245942a36a20d5fcaf46bc47817
RedGuardiaN7 <artiom.str@hotmail.it>
2023-03-29 22:57:50 +0200

    Aggiunto quarto esercizio

---
+++ exercises.js
@@ -0,0 +1,80 @@
+/* Esercizio 1 */
+
+function reverseArray(a) {
+    const reversedArray = a.reverse();
+    return reversedArray;
+}
+
+/* Esercizio 2 */
+
+function simpleArraySum(ar) {
+    // Write your code here
+    const sumWithInitial = ar.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,);
+    return sumWithInitial;
+}
+
+/* Esercizio 3 */
+
+function staircase(n) {
+    
+    for(let i = 1; i <= n; i++){
+    let space = (n-i);
+    let space_output = "";
+    for (let j = 0; j < space; j++){
+        space_output += " ";
+    }
+    
+    let print_output = "";
+    for (let j = 0; j < i; j++){
+        print_output += "#"
+    }
+    
+    console.log(space_output + print_output);
+    }
+}
+
+/* Esercizio 4 */
+
+/* Vecchio codice
+function divisibleSumPairs(n, k, ar) {
+    
+    let counter = 0;
+    
+    for (let i = 0; i <= n; i++) {
+        const copy = [...ar];
+        let first_number = copy[i];
+        copy.splice(i, 1);
+        
+        for (const second_number of copy){
+            if (first_number < second_number) {
+                let sum = first_number+second_number;
+                if (sum % k == 0){
+                    counter++;
+                }
+            }
+        }
+    }
+    
+    return counter;
+
+}
+*/
+
+function divisibleSumPairs(n, k, ar) {
+    
+    let counter = 0
+    
+    for(let i = 0; i < n; i++){
+        for(let j = i + 1; j < n ; j++){
+               
+            if((ar[i] + ar[j]) % k == 0){
+                counter++;
+            }
+        }
+    }
+    
+    return counter;
+
+}
+
