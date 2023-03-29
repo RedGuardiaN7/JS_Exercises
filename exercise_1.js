bb4a792735c6391d169823d66d960ccc56120a7c
RedGuardiaN7 <artiom.str@hotmail.it>
2023-03-29 20:48:29 +0200

    Aggiunto secondo esercizio

--- exercise_1.js
+++ exercise_1.js
@@ -1,7 +1,15 @@
 /* Esercizio 1 */
 
 function reverseArray(a) {
-    // Write your code here
     const reversedArray = a.reverse();
     return reversedArray;
+}
+
+/* Esercizio 2 */
+
+function simpleArraySum(ar) {
+    // Write your code here
+    const sumWithInitial = ar.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,);
+    return sumWithInitial;
 }
