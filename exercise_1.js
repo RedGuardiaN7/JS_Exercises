51b4b1b1259cac983bf272d91452830049125677
RedGuardiaN7 <artiom.str@hotmail.it>
2023-03-29 21:27:36 +0200

    Completato esercizio 3

--- exercise_1.js
+++ exercise_1.js
@@ -13,3 +13,23 @@ function simpleArraySum(ar) {
     (accumulator, currentValue) => accumulator + currentValue,);
     return sumWithInitial;
 }
+
+/* Esercizio 3 */
+
+function staircase(n) {
+    
+    for(let i = 1; i <= n; i++){
+    let space = (n-i);
+    let space_output = "";
+    for (let j = 0; j < space; j++){
+        space_output += " ";
+    }
+    
+    let print_output = "";
+    for (let j = 0; j < i; j++){
+        print_output += "#"
+    }
+    
+    console.log(space_output + print_output);
+    }
+}
